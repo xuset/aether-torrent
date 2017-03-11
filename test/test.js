9f733681ed3369bdff31a9025a4addac010c5af6
Austin Middleton <xuset1@gmail.com>
2017-03-10 20:49:43 -0600

    Add tests for ranges

--- test/test.js
+++ test/test.js
@@ -61,6 +61,64 @@ describe('TorrentWorker', function () {
     .then(text => assert.equal(text, 'foobar\n'))
     .then(() => tw.destroy())
   })
+
+  it('file.getStream() - ranged', function () {
+    var tw = new TorrentWorker({namespace: Math.random()})
+    tw.startSeeder()
+    return tw.add(torrentURL)
+    .then(torrent => torrent.getFile('foobar.txt').getStream({start: 2, end: 4}))
+    .then(stream => nodeStreamToString(stream))
+    .then(text => assert.equal(text, 'oba'))
+    .then(() => tw.destroy())
+  })
+
+  it('file.getBlob()', function () {
+    var tw = new TorrentWorker({namespace: Math.random()})
+    tw.startSeeder()
+    return tw.add(torrentURL)
+    .then(torrent => torrent.getFile('foobar.txt').getBlob())
+    .then(blob => blobToText(blob))
+    .then(text => assert.equal(text, 'foobar\n'))
+    .then(() => tw.destroy())
+  })
+
+  it('file.getBlob() - ranged', function () {
+    var tw = new TorrentWorker({namespace: Math.random()})
+    tw.startSeeder()
+    return tw.add(torrentURL)
+    .then(torrent => torrent.getFile('foobar.txt').getBlob({start: 2, end: 4}))
+    .then(blob => blobToText(blob))
+    .then(text => assert.equal(text, 'oba'))
+    .then(() => tw.destroy())
+  })
+
+  it('file.getWebStream()', function () {
+    if (typeof ReadableStream === 'undefined') return Promise.resolve()
+    var tw = new TorrentWorker({namespace: Math.random()})
+    tw.startSeeder()
+    return tw.add(torrentURL)
+    .then(torrent => torrent.getFile('foobar.txt').getWebStream())
+    .then(stream => {
+      assert.equal(stream.length, 7)
+      return webStreamToString(stream)
+    })
+    .then(text => assert.equal(text, 'foobar\n'))
+    .then(() => tw.destroy())
+  })
+
+  it('file.getWebStream() - ranged', function () {
+    if (typeof ReadableStream === 'undefined') return Promise.resolve()
+    var tw = new TorrentWorker({namespace: Math.random()})
+    tw.startSeeder()
+    return tw.add(torrentURL)
+    .then(torrent => torrent.getFile('foobar.txt').getWebStream({start: 2, end: 4}))
+    .then(stream => {
+      assert.equal(stream.length, 3)
+      return webStreamToString(stream)
+    })
+    .then(text => assert.equal(text, 'oba'))
+    .then(() => tw.destroy())
+  })
 })
 
 function nodeStreamToString (stream) {
@@ -75,5 +133,37 @@ function nodeStreamToString (stream) {
     stream.on('error', (err) => {
       reject(err)
     })
+  })
+}
+
+function blobToText (blob) {
+  return new Promise(function (resolve, reject) {
+    let fr = new window.FileReader()
+    fr.onload = onload
+    fr.onerror = onerror
+    fr.readAsText(blob)
+
+    function onload () {
+      resolve(fr.result)
+    }
+
+    function onerror () {
+      reject(fr.error)
+    }
+  })
+}
+
+function webStreamToString (stream) {
+  return new Promise(function (resolve, reject) {
+    let reader = stream.getReader()
+    let buffer = ''
+    reader.read().then(onRead)
+
+    function onRead (result) {
+      if (result.done) return resolve(buffer)
+
+      buffer += result.value.toString()
+      reader.read().then(onRead)
+    }
   })
 }
